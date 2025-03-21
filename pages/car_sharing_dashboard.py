999e4b4f4b541e574edbe42ec5d22f6a7b16df07
Kaichibl <kaichiboissonnade@gmail.com>
2025-03-21 10:00:09 +0000

    Merged and cleaned dataframe

---
+++ pages/car_sharing_dashboard.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+
+@st.cache_data
+def load_data():
+ trips = pd.read_csv("dataset/trips.csv")
+ cars = pd.read_csv("dataset/cars.csv")
+ cities = pd.read_csv("dataset/cities.csv")
+ return trips, cars, cities
+
+trips, cars, cities = load_data()
+
+trips_merged = trips.merge(cars, on='id', how='left')
+
+trips_merged = trips_merged.merge(cities, on='city_id', how='left')
+
+trips_merged = trips_merged.drop(columns=["id", "city_id", "id","id"])
+
+
+st.dataframe(trips_merged)
